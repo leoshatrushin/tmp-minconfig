f1852bbbdf72ed0460ecaa03d27cbc817249728e
leoshatrushin <leoshatrushin@gmail.com>
2024-06-30 13:16:31 +0800

    edits

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -3,7 +3,6 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lsp-signature-help"
         },
         config = function()
             local cmp = require("cmp")
